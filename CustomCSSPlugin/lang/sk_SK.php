e4ff51909b73c7a155352a301b98b695f5974f35
Bokor Pavol <bokor.pavol@gmail.com>
2020-06-25 13:02:06 +0200

    Update sk_SK.php

--- CustomCSSPlugin/lang/sk_SK.php
+++ CustomCSSPlugin/lang/sk_SK.php
@@ -2,9 +2,9 @@
 /**
  * Slovak Language File for Custom CSS Plugin
  *
- * Date:		24 June 2020
+ * Date:	24 June 2020
  * Revision:	0.1	
- * Version:		GetSimple 3.3
+ * Version:	GetSimple 3.3
  * Traductors: 	Pavol Bokor 
  *
  * @package GetSimple
