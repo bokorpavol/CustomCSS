22eef3a10964343f74c9e30d78724013276b3796
Bokor Pavol <bokor.pavol@gmail.com>
2020-06-25 13:00:20 +0200

    Create en_US.php

---
+++ CustomCSSPlugin/lang/en_US.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * English Language File for Custom CSS Plugin
+ *
+ * Date:		24 June 2020
+ * Revision:	0.1
+ * Version:		GetSimple 3.3
+ * Traductors: 	Pavol Bokor 
+ *
+ * @package GetSimple
+ * @subpackage Language
+ */
+ 
+$i18n = array (
+	
+	"CUSTOMCSS_TITLE"	=>	"Custom CSS",
+	"CUSTOMCSS_DESC"	=>	"Add custom CSS to your theme"
+);
