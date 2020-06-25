891b8c7ee34b4275d01153cf139a17c972e23db1
Bokor Pavol <bokor.pavol@gmail.com>
2020-06-25 13:01:24 +0200

    Add files via upload

---
+++ CustomCSSPlugin/lang/sk_SK.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Slovak Language File for Custom CSS Plugin
+ *
+ * Date:		24 June 2020
+ * Revision:	0.1	
+ * Version:		GetSimple 3.3
+ * Traductors: 	Pavol Bokor 
+ *
+ * @package GetSimple
+ * @subpackage Language
+ */
+ 
+$i18n = array (
+	
+	"CUSTOMCSS_TITLE"	=>	"Custom CSS",
+	"CUSTOMCSS_DESC"	=>	"Pridá vlastné CSS do motívu"
+);
